8c9710a13536d552648df6f04f3934410690932a
Imre Lindi <imre.lindi@codecool.com>
2016-10-24 11:54:37 +0200

    doors.py

---
+++ doors.py
@@ -0,0 +1,16 @@
+def change_door_state(closed, open):
+    if closed:
+        return open
+    elif open:
+        return closed
+
+doors(1, 100) = closed
+
+repeats = 1
+
+while True:
+    repeats < 101
+    change_door_state
+    repeats = repeats + 1
+    
+print((doors(1,100))
